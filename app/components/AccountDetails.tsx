28481e981124e466dfce299f724a8bd3debd76f0
Elton <Liao.elton@gmail.com>
2023-11-07 11:39:17 +0800

    feat: change layout

--- app/components/AccountDetails.tsx
+++ app/components/AccountDetails.tsx
@@ -1,3 +1,10 @@
+"use client";
+import { usePrepareUserOperation, useSendUserOperation } from "moonchute";
+import { useEffect } from "react";
+import { useAccount } from "wagmi";
+import SampleNFTABI from "../SampleNFT.json";
+import Loader from "./Loader";
+
 export default function AccountDetails({
   account,
   index,
@@ -5,15 +12,59 @@ export default function AccountDetails({
   account: any;
   index: number;
 }) {
+  const { address } = useAccount();
+  const {
+    config,
+    error,
+    isLoading: isPreparing,
+    isError: puoIsError,
+  } = usePrepareUserOperation({
+    address: "0x34bE7f35132E97915633BC1fc020364EA5134863",
+    account: account.address as `0x${string}`,
+    abi: SampleNFTABI.abi,
+    functionName: "mint",
+    args: [account.address || address],
+  });
+
+  const {
+    data: userOpHash,
+    write,
+    isLoading: isUserOpLoading,
+    isError: suoIsError,
+  } = useSendUserOperation(config);
+
+  useEffect(() => {
+    if (userOpHash) {
+      console.log(userOpHash);
+      alert(`Mint successfully! userOp hash: ${userOpHash.userOpHash}`);
+    }
+  }, [userOpHash]);
+
   return (
-    <div className="w-full h-full flex flex-col gap-2">
-      <div className="font-semibold">{`Smart Account ${index + 1}`}</div>
-      <div>
-        {`address: ${account.address.slice(0, 6)}...${account.address.slice(
-          -4
-        )}`}
+    <div className="w-full h-full flex flex-col gap-2 justify-between">
+      <div className="p-4">
+        <div className="font-semibold">{`Smart Account ${index + 1}`}</div>
+        <div>
+          {`address: ${account.address.slice(0, 6)}...${account.address.slice(
+            -4
+          )}`}
+        </div>
+        <div>{`provider: ${account.provider}`}</div>
       </div>
-      <div>{`provider: ${account.provider}`}</div>
+      <button
+        className="flex justify-center rounded-b-xl w-full bg-black hover:bg-transparent  text-white font-semibold hover:text-black py-2 px-4 border-t-2 border-black"
+        onClick={() => {
+          if (write) write();
+        }}
+      >
+        {isPreparing ? (
+          "Prepare your userop"
+        ) : isUserOpLoading ? (
+          <Loader />
+        ) : (
+          "Mint! (with userop)"
+        )}
+      </button>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import {
-  usePrepareUserOperation,
-  useSendUserOperation,
-  useSmartAccounts,
-} from "moonchute";
+import { useCreateSmartAccount, useSmartAccounts } from "moonchute";
+import { useEffect, useState } from "react";
 import { useAccount, useNetwork } from "wagmi";
 import SampleNFTABI from "./SampleNFT.json";
 import AccountDetails from "./components/AccountDetails";
@@ -13,53 +10,70 @@ import CardLayout from "./components/CardLayout";
 import Loader from "./components/Loader";
 
 export default function RetrieveAccounts() {
+  const [hydration, setHydration] = useState<boolean>(false);
   const { address } = useAccount();
   const { chain } = useNetwork();
-  const { data: accounts, isLoading } = useSmartAccounts({
+  const {
+    data: accounts,
+    isLoading,
+    isError,
+  } = useSmartAccounts({
     address: address,
     chainId: chain?.id,
   });
-  const { config, isError: puoIsError } = usePrepareUserOperation({
-    address: "0x34bE7f35132E97915633BC1fc020364EA5134863",
-    account: accounts?.smartAccount?.[0]?.address as `0x${string}`,
-    abi: SampleNFTABI.abi,
-    functionName: "mint",
-    args: [accounts?.smartAccount?.[0]?.address || address],
-  });
 
   const {
-    data: userOpHash,
-    write,
-    isError: suoIsError,
-  } = useSendUserOperation(config);
+    data: createUserOp,
+    write: createWrite,
+    isLoading: createIsLoading,
+    isError: createIsError,
+  } = useCreateSmartAccount({
+    abi: SampleNFTABI.abi,
+    address: "0x34bE7f35132E97915633BC1fc020364EA5134863",
+    functionName: "mint",
+    args: ["0x6136b647C9971f1EDc7641e14a9E0Ca7b2626080"],
+  });
 
+  useEffect(() => {
+    setHydration(true);
+  }, []);
+
+  useEffect(() => {
+    if (createUserOp) {
+      console.log(createUserOp);
+      alert(`Create successfully! userOp hash: ${createUserOp.userOpHash}`);
+    }
+  }, [createUserOp]);
+
+  if (!hydration) return null;
   return (
-    <>
-      <h1 className="text-2xl font-bold">Retrieve your smart accounts:</h1>
+    <div className="flex flex-col gap-4 items-center miw-w-full md:min-w-3/4 bg-[#F6BD41] border-zinc-900 dark:border-0 border-2 rounded-lg p-4 md:p-6 mt-2">
+      <h1 className="text-2xl font-bold">Small smart account manager</h1>
+      <ConnectButton />
       <button
+        className={`min-w-[200px] flex justify-center rounded-xl ${
+          !createIsLoading ? "bg-white" : "bg-zinc-200"
+        } shadow-sm shadow-zinc-300 hover:scale-105 transition-all font-semibold text-black py-2 px-4`}
         onClick={() => {
-          if (write) write();
+          if (createWrite) createWrite();
         }}
+        disabled={createIsLoading}
       >
-        Send UserOperation
+        {createIsLoading ? <Loader /> : "Create Smart Account"}
       </button>
-      {userOpHash && <p>UserOp Hash: {userOpHash.userOpHash}</p>}
-      <div className="flex flex-col items-center miw-w-full md:min-w-3/4 bg-[#F6BD41] border-zinc-900 dark:border-0 border-2 rounded-lg p-6 md:p-10 mt-2">
-        <ConnectButton />
-        <div className="grid grid-cols-1 my-12 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
-          {isLoading
-            ? [...Array(3)].map((_, index) => (
-                <CardLayout key={index}>
-                  <Loader />
-                </CardLayout>
-              ))
-            : accounts?.smartAccount?.map((account: any, index: number) => (
-                <CardLayout key={index}>
-                  <AccountDetails account={account} index={index} />
-                </CardLayout>
-              ))}
-        </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+        {isLoading
+          ? [...Array(3)].map((_, index) => (
+              <CardLayout key={index}>
+                <Loader />
+              </CardLayout>
+            ))
+          : accounts?.smartAccount?.map((account: any, index: number) => (
+              <CardLayout key={index}>
+                <AccountDetails account={account} index={index} />
+              </CardLayout>
+            ))}
       </div>
-    </>
+    </div>
   );
 }
